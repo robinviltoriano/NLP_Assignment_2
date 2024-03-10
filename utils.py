4663bbcbbfdfa62c8fc34a1271f975868e8fc530
robinviltoriano <robin.vito3@gmail.com>
2024-03-10 15:39:00 +1030

    add utils python

--- utils.py
+++ utils.py
@@ -1,14 +1,8 @@
 import re
 
 def clean_text(text):
-    # Remove non-ASCII characters
-    text = ''.join([char for char in text if ord(char) < 128])
-
-    # Remove multiple spaces
-    text = re.sub(r'\s+', ' ', text)
-
-    # Remove question mark problems
     text = re.sub(r'(\s\?)',' ',text)
+    text = re.sub(r'\s+', ' ', text)
     text = re.sub(r"\b\?\b", "\'", text)
     text = re.sub(r"(,\?)",",", text)
     text = re.sub(r"\?+", "?", text)
