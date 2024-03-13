b61a00679058f30515825f2ae16ba8e463d99af8
jamesphm04 <jamesphm449@gmail.com>
2024-03-13 13:41:01 +1030

    create rag chain python file

---
+++ rag_chain.py
@@ -0,0 +1,86 @@
+from langchain_community.embeddings.huggingface import HuggingFaceInstructEmbeddings
+from langchain_community.llms.huggingface_hub import HuggingFaceHub
+from langchain_community.vectorstores.faiss import FAISS
+from PyPDF2 import PdfReader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.memory import ConversationBufferWindowMemory
+from langchain.chains import ConversationChain
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import AIMessage, HumanMessage
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
+
+
+
+
+model = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":1000})
+
+def get_rag_chain(context):
+    """
+    Des: This function returns a RAG chain which can be used to answer questions based on the given context.
+    Params:
+    * context: any
+    
+    Example usecase:
+        chat_history = []
+
+        question = "What is the weight of this assignment?"
+        ai_msg = rag_chain.invoke({"question": question, "chat_history": chat_history})
+        chat_history.extend([HumanMessage(content=question), ai_msg])
+
+        second_question = "How many of them is from report?"
+        ai_msg = rag_chain.invoke({"question": second_question, "chat_history": chat_history})
+        chat_history.extend([HumanMessage(content=second_question), ai_msg])
+    """
+    
+    def get_context():
+        #Thinh's part
+        pass
+    
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+                                    which might reference context in the chat history, formulate a standalone question \
+                                    which can be understood without the chat history. Do NOT answer the question, \
+                                    just reformulate it if needed and otherwise return it as is. But the most important thing, you have to answer using natural language as a human.{chat_history}"""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
+    )
+    contextualize_q_chain = contextualize_q_prompt | model | StrOutputParser()
+    
+    
+    def contextualized_question(input: dict):
+        if input.get("chat_history"):
+            return contextualize_q_chain
+        else:
+            return input["question"]
+    
+    qa_system_prompt = """You are an assistant for question-answering tasks. \
+                        Use the following pieces of retrieved context to answer the question. \
+                        If you don't know the answer, just say that you don't know. But the most important thing, you have to answer using natural language as a human.\
+
+                        {context}"""
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder(variable_name="chat_history"),
+            ("human", "{question}"),
+        ]
+    )
+
+    rag_chain = (
+        RunnablePassthrough.assign(
+            context=contextualized_question | get_context | format_docs
+        )
+        | qa_prompt
+        | model
+    )
+    
+    return rag_chain
+    
